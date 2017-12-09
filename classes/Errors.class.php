8c3d47ccb0828b2fa807ba1ca0ce53bc42db2c6e
sameeullahqazi <samee@lamproslabs.com>
2017-12-09 18:57:57 +0500

    classes
    All classes

--- classes/Errors.class.php
+++ classes/Errors.class.php
@@ -6,28 +6,28 @@ class Errors
 	public static function show404()
 	{
 		header("HTTP/1.1 404 Not Found");
-		require_once("404.html");
+		require_once(dirname(__DIR__) ."/404.html");
 		exit;
 	}
 	
 	public static function show500()
 	{
 		header("HTTP/1.1 500 Server Error");
-		require_once("500.html");
+		require_once(dirname(__DIR__) ."/500.html");
 		exit;
 	}
 	
 	public static function show600()
 	{
 		header("HTTP/1.1 600 Server Error");
-		require_once("600.html");
+		require_once(dirname(__DIR__) ."/600.html");
 		exit;
 	}
 	
 	public static function show700()
 	{
 		header("HTTP/1.1 700 Server Error");
-		require_once("700.html");
+		require_once(dirname(__DIR__) ."/700.html");
 		exit;
 	}
 }
